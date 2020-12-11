5b36210c4602edda17c8c9b270acb7865df94a93
aaravrajesh007 <aaravrajesh007@gmail.com>
2020-12-11 17:41:21 +0530

    modified

--- Bird.js
+++ Bird.js
@@ -2,6 +2,9 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+
+    this.path=[];
   }
 
   display() {
@@ -9,5 +12,12 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x>10&&this.body.position.x>230){
+    var position = [this.body.position.x,this.body.position.y];
+    this.path.push(position);}
+
+    for(var i=0;i<this.path.length;i=i+1){
+      image(this.smokeimage,this.path[i][0],this.path[i][1])
+    }
   }
 }
